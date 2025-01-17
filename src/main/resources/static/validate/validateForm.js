0f029659a8e9470439e96c63210c8c7fd5aa5272
ITWeiss <gottfried98@bk.ru>
2025-01-17 22:37:26 +0700

    HTML/CSS/JS, Freemarker Lesson 19

---
+++ src/main/resources/static/validate/validateForm.js
@@ -0,0 +1,9 @@
+// Функция для проверки ввода имени
+function validateForm() {
+    var name = document.getElementById("name").value;
+    if (name === "") {
+        alert("Пожалуйста, введите имя.");
+        return false; // Останавливает отправку формы
+    }
+    return true; // Разрешает отправку формы
+}
